84df5e2944ce2f29f723437e7bfbbd49ede47e9e
David <davidmartinezmit@gmail.com>
2022-02-18 21:05:46 +0100

    Afegim preguntes noves

--- app/src/main/java/com/androidpprog2/com/MainActivity.java
+++ app/src/main/java/com/androidpprog2/com/MainActivity.java
@@ -3,13 +3,33 @@ package com.androidpprog2.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
     }
 
